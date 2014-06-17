158848ce3d9e3df83aadf2d095bec535fa28ee41
Spencer Alger <spencer@spenceralger.com>
2014-06-17 10:24:20 -0700

    using promises

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var async = require('async');
-
+var Promise = require('bluebird');
 var client = require('./_client');
 var randomEvent = require('./_randomEvent');
 var samples = require('./samples');
@@ -13,52 +12,43 @@ var endingMoment = argv.end;
 console.log('Generating', total, 'events from', startingMoment.format(), 'to', endingMoment.format());
 if (argv.dry) process.exit();
 
-async.series([
-  function (done) {
-    if (argv.reset) {
-      argv.log('clearing existing logstash-* indices');
-      client.indices.delete({
-        index: 'logstash-*'
-      }, done);
-    } else {
-      done();
-    }
-  },
-  function (done) {
-    argv.log('setting the bulk threadpool size to unlimited');
-    client.cluster.putSettings({
-      body: {
-        transient: {
-          threadpool: {
-            bulk: {
-              queue_size: -1
-            }
+Promise.resolve()
+.then(function () {
+  if (argv.reset) {
+    argv.log('clearing existing logstash-* indices');
+    return client.indices.delete({
+      index: 'logstash-*'
+    });
+  }
+})
+.then(function () {
+  argv.log('setting the bulk threadpool size to unlimited');
+  return client.cluster.putSettings({
+    body: {
+      transient: {
+        threadpool: {
+          bulk: {
+            queue_size: -1
           }
         }
       }
-    }, done);
-  },
-  function (done) {
-    async.timesSeries(total, function (i, done) {
-      var event = randomEvent();
+    }
+  });
+})
+.then(function () {
+  var i = total;
+  return (function crunch(event) {
+    if (--i <= -1) return;
+    eventBuffer.push({
+      header: { _index: event.index, _type: samples.types(), _id: i, },
+      body: event
+    });
 
-      eventBuffer.push({
-        header: {
-          _index: event.index,
-          _type: samples.types(),
-          _id: i,
-        },
-        body: event
-      });
-
-      // setImmediate because this event creation loop would be sync
-      // otherwise, we need to give the buffer and client time to send
-      // their events
-      setImmediate(done);
-    }, done);
-  }
-], function (err) {
-  if (err) throw err;
-  // alert the event buffer that we are not going to be writing any more
+    // async so that there is time to process the queue
+    return Promise.resolve(randomEvent()).then(crunch);
+  }(randomEvent()));
+})
+.then(function () {
   eventBuffer.push(false);
-});
+})
+.catch(console.error.bind(console));
