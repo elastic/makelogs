ca23e38dece3abf42163a049defcbff3908c9f8a
Spencer Alger <spencer@spenceralger.com>
2014-09-24 13:19:41 -0700

    fixed ref to package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Notify user of updates
  */
-var pkg = require('../package.json');
+var pkg = require('./package.json');
 require('update-notifier')({ packageName: pkg.name, packageVersion: pkg.version }).notify();
 
 var Promise = require('bluebird');
