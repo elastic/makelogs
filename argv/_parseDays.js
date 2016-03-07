9206254ac7d2e41db6714e27e71f950a4fb47987
spalger <spalger@users.noreply.github.com>
2016-03-07 12:43:29 -0700

    simplify --days flag based on feedback from @itayw

--- argv/_parseDays.js
+++ argv/_parseDays.js
@@ -1,36 +1,25 @@
 var _ = require('lodash');
 var moment = require('moment');
 
-module.exports = function parseDays(argv) {
-  var complexDateRE = /(\-|\+|)\d+,?/g;
-  var moments = [];
+var customDayBoundsRE = /^\d+\/\d+$/g;
 
+module.exports = function parseDays(argv) {
   var startBase = moment().utc().startOf('day');
   var endBase = moment().utc().endOf('day');
 
-
   if (_.isNumber(argv.days)) {
-    moments.push(startBase.subtract(argv.days, 'days'));
-    moments.push(endBase.add(argv.days, 'days'));
-    return moments;
+    return [
+      startBase.subtract(argv.days, 'days'),
+      endBase.add(argv.days, 'days')
+    ];
   }
 
-  if (_.isString(argv.days) && argv.days.match(complexDateRE)) {
-    var ends = [], match;
-    while (ends.length < 2 && (match = complexDateRE.exec(argv.days))) {
-      ends.push(match);
-    }
-
-    if (ends.length === 1) {
-      ends.push(ends[0]);
-    }
-
-    if (ends.length === 2) {
-      ends = ends.slice(0, 2).map(function (n) { return Math.abs(parseFloat(n)); });
-      moments.push(startBase.subtract(ends[0], 'days'));
-      moments.push(endBase.add(ends[1], 'days'));
-      return moments;
-    }
+  if (_.isString(argv.days) && argv.days.match(customDayBoundsRE)) {
+    var ends = argv.days.split('/').map(parseFloat);
+    return [
+      startBase.subtract(ends[0], 'days'),
+      endBase.add(ends[1], 'days')
+    ];
   }
 
   throw new TypeError('Unable to determine the starting and end dates.');
--- argv/index.js
+++ argv/index.js
@@ -17,8 +17,9 @@ var optimist = require('optimist')
       alias: 'd',
       type: 'number',
       required: true,
-      describe: 'The number of days ± today or two numbers, seperated by a comma' +
-        ', like "-1,+10" or "-10,+100"',
+      describe: 'The number of days ± today or two numbers seperated by a slash to represent ' +
+        'the number of days back/forward data should be generated for' +
+        ', e.g. "1/10" to generate back one day, and forward 10',
       default: 1
     },
     host: {
