f4675924b05148a8ff242325b678036159f5bdee
spalger <spalger@users.noreply.github.com>
2016-03-07 12:47:45 -0700

    add warning about --days format change

--- argv/_parseDays.js
+++ argv/_parseDays.js
@@ -1,7 +1,8 @@
 var _ = require('lodash');
 var moment = require('moment');
 
-var customDayBoundsRE = /^\d+\/\d+$/g;
+var customDayBoundsRE = /^\d+\/\d+$/;
+var oldDayExpressionRE = /[\-\+\,]/;
 
 module.exports = function parseDays(argv) {
   var startBase = moment().utc().startOf('day');
@@ -14,12 +15,17 @@ module.exports = function parseDays(argv) {
     ];
   }
 
-  if (_.isString(argv.days) && argv.days.match(customDayBoundsRE)) {
-    var ends = argv.days.split('/').map(parseFloat);
-    return [
-      startBase.subtract(ends[0], 'days'),
-      endBase.add(ends[1], 'days')
-    ];
+  if (_.isString(argv.days)) {
+    if (customDayBoundsRE.test(argv.days)) {
+      var ends = argv.days.split('/').map(parseFloat);
+      return [
+        startBase.subtract(ends[0], 'days'),
+        endBase.add(ends[1], 'days')
+      ];
+    }
+    else if (oldDayExpressionRE.test(argv.days)) {
+      throw new TypeError('the format of the --days flag has changed. run `makelogs --help` for more info.')
+    }
   }
 
   throw new TypeError('Unable to determine the starting and end dates.');
