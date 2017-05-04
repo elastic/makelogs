799a6ff0bd74f48b28fdfc61d09c70f9af12eaf9
spalger <spalger@users.noreply.github.com>
2017-05-04 15:27:42 -0700

    remove unused dest directory

--- dest/samples/os.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = ['osx', 'ios', 'win xp', 'win 7', 'win 8'];
-//# sourceMappingURL=os.js.map
--- dest/samples/round_all_gets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = function (stoch) {
-  return function () {
-    return Math.round(stoch.get.apply(stoch, arguments));
-  };
-};
-//# sourceMappingURL=round_all_gets.js.map
