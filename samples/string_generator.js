7d5555e06a95750748a5fcd5f101e80a41edc6c5
Tim Roes <mail@timroes.de>
2018-06-22 17:00:34 +0200

    Tabs to spaces

--- samples/string_generator.js
+++ samples/string_generator.js
@@ -1,9 +1,9 @@
 module.exports = function stringGenerator (length, withSpaces) {
-	var chars = 'abcdefghijklmnopqrstuvwxyz';
-	if (withSpaces) {
-		chars += ' ';
-	}
-	return new Array(parseInt(length)).fill(0).map(function() {
-		return chars.charAt(Math.floor(Math.random() * chars.length));
-	}).join('');
+  var chars = 'abcdefghijklmnopqrstuvwxyz';
+  if (withSpaces) {
+    chars += ' ';
+  }
+  return new Array(parseInt(length)).fill(0).map(function() {
+    return chars.charAt(Math.floor(Math.random() * chars.length));
+  }).join('');
 };
