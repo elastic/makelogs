8fa3ffec15c65f0468e8a13e73cfe29970a7878c
Roger <ruggero.schirinzi@gmail.com>
2023-01-17 09:42:03 +0100

    new version

--- argv/_parseTime.js
+++ argv/_parseTime.js
@@ -6,25 +6,96 @@ var customDayBoundsRE = /^\-?\d+\/\-?\d+$/;
 var oldDayExpressionRE = /[\-\+\,]/;
 
 module.exports = function parseDays(argv) {
-  var startBase = moment().utc().startOf(argv.timeWindowType);
-  var endBase = moment().utc().endOf(argv.timeWindowType);
-  // console.log('startBase', startBase, 'endBase', endBase);
-  if (typeof argv.timeWindowValue === 'number') {
-    return [
-      startBase.subtract(argv.timeWindowValue, argv.timeWindowType),
-      endBase.add(argv.timeWindowValue, argv.timeWindowType)
-    ];
+  // split argv.time with / char ----> 3d/2h
+  // check last char of argv.time[0] to get hour minute days and set in new var1
+  // check last char of argv.time[1] to get hour minute days and set in new var2
+  // continue replacing argv.timeWindowType with new var1 and new var2
+  var splitTime = argv.time.split("/");
+  var startBaseTmp = splitTime[0];
+  var startBaseTypeTmp = startBaseTmp.at(-1);
+  
+  switch (startBaseTypeTmp) {
+    case 'd':
+      var timeType = "day";
+      var startBaseValueTmp = startBaseTmp.slice(0, -1);
+      break;
+    case 'h':
+      var timeType = "hour";
+      var startBaseValueTmp = startBaseTmp.slice(0, -1);
+      break;
+    case 'm':
+      var timeType = "minute";
+      var startBaseValueTmp = startBaseTmp.slice(0, -1);
+      break;
+    case 'w':
+      var timeType = "week";
+      var startBaseValueTmp = startBaseTmp.slice(0, -1);
+      break;
+    case 'y':
+      var timeType = "year";
+      var startBaseValueTmp = startBaseTmp.slice(0, -1);
+      break;
+    default:
+      var timeType = "day";
+      var startBaseValueTmp = startBaseTmp;
+      break;
   }
 
-  else if (typeof argv.timeWindowValue === 'string') {
-    if (customDayBoundsRE.test(argv.timeWindowValue)) {
-      var ends = argv.timeWindowValue.split('/').map(parseFloat);
+  if (typeof splitTime[1] === "undefined"){
+    endBaseTypeTmp = "d";
+    endBaseValueTmp = 0;
+    endTimeType = "d";
+  }
+  else {
+    var endBaseTmp = splitTime[1];
+    var endBaseTypeTmp = endBaseTmp.at(-1);
+    switch (endBaseTypeTmp) {
+      case 'd':
+        var endTimeType = "day";
+        var endBaseValueTmp = endBaseTmp.slice(0, -1);
+        break;
+      case 'h':
+        var endTimeType = "hour";
+        var endBaseValueTmp = endBaseTmp.slice(0, -1);
+        break;
+      case 'm':
+        var endTimeType = "minute";
+        var endBaseValueTmp = endBaseTmp.slice(0, -1);
+        break;
+      case 'w':
+        var endTimeType = "week";
+        var endBaseValueTmp = endBaseTmp.slice(0, -1);
+        break;
+      case 'y':
+        var endTimeType = "year";
+        var endBaseValueTmp = endBaseTmp.slice(0, -1);
+        break;
+      default:
+        var endTimeType = "day";
+        var endBaseValueTmp = endBaseTmp;
+        break;
+    }
+  }
+
+
+  var startBase = moment().utc().startOf(timeType);
+  var endBase = moment().utc().endOf(endTimeType);
+  // console.log('startBase', startBase, 'endBase', endBase);
+  if (typeof argv.time === 'number') {
+    return [
+      startBase.subtract(argv.time, timeType),
+      endBase.add(argv.time, endTimeType)
+    ];
+  }
+  else if (typeof argv.time === 'string') {
+    if (customDayBoundsRE.test(argv.time)) {
+      var ends = argv.time.split('/').map(parseFloat);
       return [
-        startBase.subtract(ends[0], argv.timeWindowType),
-        endBase.add(ends[1], argv.timeWindowType)
+        startBase.subtract(ends[0], timeType),
+        endBase.add(ends[1], endTimeType)
       ];
     }
-    else if (oldDayExpressionRE.test(argv.timeWindowValue)) {
+    else if (oldDayExpressionRE.test(argv.time)) {
       throw new TypeError('the format of the --days flag has changed. run `makelogs --help` for more info.')
     }
   }
